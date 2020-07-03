0eacbed9d7595870de5a2de1d765d8992763e5fd
Igor Chorążewicz <igor.chorazewicz@intel.com>
2020-07-03 13:56:39 +0200

    XXX

--- tests/radix/radix.cpp
+++ tests/radix/radix.cpp
@@ -7,6 +7,21 @@
 
 #include <libpmemobj++/inline_string.hpp>
 
+#include <chrono>
+
+template <typename TimeUnit, typename F>
+static typename TimeUnit::rep
+measure(F &&func)
+{
+	auto start = std::chrono::steady_clock::now();
+
+	func();
+
+	auto duration = std::chrono::duration_cast<TimeUnit>(
+		std::chrono::steady_clock::now() - start);
+	return duration.count();
+}
+
 namespace nvobj = pmem::obj;
 
 using container = nvobj::radix_tree<int>;
@@ -26,78 +41,96 @@ test_emplace(nvobj::pool<root> &pop)
 
 	nvobj::transaction::run(pop, [&] {
 		r->radix = nvobj::make_persistent<container>();
-		r->radix->emplace("", 0);
-		r->radix->emplace("ab", 1);
-		r->radix->emplace("ba", 2);
-		r->radix->emplace("a", 3);
-		r->radix->emplace("b", 4);
+		// r->radix->emplace("", 0);
+		// r->radix->emplace("ab", 1);
+		// r->radix->emplace("ba", 2);
+		// r->radix->emplace("a", 3);
+		// r->radix->emplace("b", 4);
 
-		r->radix_in = nvobj::make_persistent<container_in>();
-		r->radix_in->emplace("", "");
-		r->radix_in->emplace(" ", "ab");
-		r->radix_in->emplace("  ", "ab");
+		//r->radix_in = nvobj::make_persistent<container_in>();
 
-		r->radix_in->emplace("ab", "ab");
-		r->radix_in->emplace("ba", "ba");
-		r->radix_in->emplace("a", "a");
-		r->radix_in->emplace("b", "b");
+		// pmem::obj::actions acts(pop, g_pool_id);
+
+		// r->radix_in->emplace(acts, "", "x");
+		// r->radix_in->emplace(acts, "ab", "ab");
+		// r->radix_in->emplace(acts, "ba", "ba");
+		// r->radix_in->emplace(acts, "a", "a");
+		// r->radix_in->emplace(acts, "b", "b");
+
+		// r->radix_in->print();
+
+		// acts.publish();
+
+		// r->radix_in->print();
 	});
 
-	auto it = r->radix->find("a");
-	UT_ASSERT(it.key().compare(std::string("a")) == 0);
-	UT_ASSERTeq(it.value(), 3);
+	std::cout << measure<std::chrono::milliseconds>([&] {
+		nvobj::transaction::run(pop, [&] {
 
-	++it;
-	UT_ASSERT(it.key().compare(std::string("ab")) == 0);
-	UT_ASSERTeq(it.value(), 1);
+		for (size_t i = 0; i < 2861; i++)
+			r->radix->emplace(pmem::obj::string_view((char*)&i, 4), i);
 
-	++it;
-	UT_ASSERT(it.key().compare(std::string("b")) == 0);
-	UT_ASSERTeq(it.value(), 4);
+		});
+	}) << "ms" << std::endl;
 
-	++it;
-	UT_ASSERT(it.key().compare(std::string("ba")) == 0);
-	UT_ASSERTeq(it.value(), 2);
+	for (size_t i = 0; i < 2861; i++)
+		UT_ASSERT(r->radix->find(pmem::obj::string_view((char*)&i, 4)).value() == i);
 
-	--it;
-	UT_ASSERT(it.key().compare(std::string("b")) == 0);
-	UT_ASSERTeq(it.value(), 4);
+	// auto it = r->radix->find("a");
+	// UT_ASSERT(it.key().compare(std::string("a")) == 0);
+	// UT_ASSERTeq(it.value(), 3);
 
-	--it;
-	UT_ASSERT(it.key().compare(std::string("ab")) == 0);
-	UT_ASSERTeq(it.value(), 1);
+	// ++it;
+	// UT_ASSERT(it.key().compare(std::string("ab")) == 0);
+	// UT_ASSERTeq(it.value(), 1);
 
-	--it;
-	UT_ASSERT(it.key().compare(std::string("a")) == 0);
-	UT_ASSERTeq(it.value(), 3);
+	// ++it;
+	// UT_ASSERT(it.key().compare(std::string("b")) == 0);
+	// UT_ASSERTeq(it.value(), 4);
 
-	--it;
-	UT_ASSERT(it.key().compare(std::string("")) == 0);
-	UT_ASSERTeq(it.value(), 0);
+	// ++it;
+	// UT_ASSERT(it.key().compare(std::string("ba")) == 0);
+	// UT_ASSERTeq(it.value(), 2);
 
-	it = r->radix->erase(it);
-	UT_ASSERT(it.key().compare(std::string("a")) == 0);
-	UT_ASSERTeq(it.value(), 3);
+	// --it;
+	// UT_ASSERT(it.key().compare(std::string("b")) == 0);
+	// UT_ASSERTeq(it.value(), 4);
 
-	(*it).second = 4;
-	UT_ASSERT(it.key().compare(std::string("a")) == 0);
-	UT_ASSERTeq(it.value(), 4);
+	// --it;
+	// UT_ASSERT(it.key().compare(std::string("ab")) == 0);
+	// UT_ASSERTeq(it.value(), 1);
 
-	it = r->radix->lower_bound("b");
-	UT_ASSERT(it.key().compare(std::string("b")) == 0);
+	// --it;
+	// UT_ASSERT(it.key().compare(std::string("a")) == 0);
+	// UT_ASSERTeq(it.value(), 3);
 
-	it = r->radix->lower_bound("aa");
-	UT_ASSERT(it.key().compare(std::string("ab")) == 0);
+	// --it;
+	// UT_ASSERT(it.key().compare(std::string("")) == 0);
+	// UT_ASSERTeq(it.value(), 0);
 
-	auto it2 = r->radix_in->lower_bound("aa");
-	it2.assign("xx");
+	// it = r->radix->erase(it);
+	// UT_ASSERT(it.key().compare(std::string("a")) == 0);
+	// UT_ASSERTeq(it.value(), 3);
 
-	auto long_string = std::string(1024, 'x');
-	it2.assign(long_string);
+	// (*it).second = 4;
+	// UT_ASSERT(it.key().compare(std::string("a")) == 0);
+	// UT_ASSERTeq(it.value(), 4);
 
-	UT_ASSERT(r->radix_in->find("") != r->radix_in->end());
-	UT_ASSERT(r->radix_in->find(" ") != r->radix_in->end());
-	UT_ASSERT(r->radix_in->find(" ") != r->radix_in->end());
+	// it = r->radix->lower_bound("b");
+	// UT_ASSERT(it.key().compare(std::string("b")) == 0);
+
+	// it = r->radix->lower_bound("aa");
+	// UT_ASSERT(it.key().compare(std::string("ab")) == 0);
+
+	// auto it2 = r->radix_in->lower_bound("aa");
+	// it2.assign("xx");
+
+	// auto long_string = std::string(1024, 'x');
+	// it2.assign(long_string);
+
+	// UT_ASSERT(r->radix_in->find("") != r->radix_in->end());
+	// UT_ASSERT(r->radix_in->find(" ") != r->radix_in->end());
+	// UT_ASSERT(r->radix_in->find(" ") != r->radix_in->end());
 }
 }
 
