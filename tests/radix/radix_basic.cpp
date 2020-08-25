4260422ddc8ef80c595633201390f8e9a0a8291e
Igor Chorążewicz <igor.chorazewicz@intel.com>
2020-08-25 16:41:33 +0200

    XXX

--- tests/radix/radix_basic.cpp
+++ tests/radix/radix_basic.cpp
@@ -3,6 +3,7 @@
 
 #include "radix.hpp"
 
+#include <algorithm>
 #include <random>
 #include <unordered_map>
 #include <unordered_set>
@@ -10,295 +11,6 @@
 #include <sstream>
 
 static std::mt19937_64 generator;
-
-void
-test_iterators(nvobj::pool<root> &pop)
-{
-	auto r = pop.root();
-
-	nvobj::transaction::run(pop, [&] {
-		r->radix_int = nvobj::make_persistent<container_int>();
-		r->radix_int->try_emplace("", 0U);
-		r->radix_int->try_emplace("ab", 1U);
-		r->radix_int->try_emplace("ba", 2U);
-		r->radix_int->try_emplace("a", 3U);
-		r->radix_int->try_emplace("b", 4U);
-
-		r->radix_str = nvobj::make_persistent<container_string>();
-		r->radix_str->try_emplace("", "");
-
-		r->radix_str->try_emplace(" ", "ab");
-		r->radix_str->try_emplace("  ", "ab");
-
-		r->radix_str->try_emplace("ab", "ab");
-		r->radix_str->try_emplace("ba", "ba");
-		r->radix_str->try_emplace("a", "a");
-		r->radix_str->try_emplace("b", "b");
-	});
-
-	auto it = r->radix_int->find("a");
-	UT_ASSERT(nvobj::string_view(it->key()).compare(std::string("a")) == 0);
-	UT_ASSERTeq(it->value(), 3);
-
-	++it;
-	UT_ASSERT(nvobj::string_view(it->key()).compare(std::string("ab")) ==
-		  0);
-	UT_ASSERTeq(it->value(), 1);
-
-	++it;
-	UT_ASSERT(nvobj::string_view(it->key()).compare(std::string("b")) == 0);
-	UT_ASSERTeq(it->value(), 4);
-
-	++it;
-	UT_ASSERT(nvobj::string_view(it->key()).compare(std::string("ba")) ==
-		  0);
-	UT_ASSERTeq(it->value(), 2);
-
-	--it;
-	UT_ASSERT(nvobj::string_view(it->key()).compare(std::string("b")) == 0);
-	UT_ASSERTeq(it->value(), 4);
-
-	--it;
-	UT_ASSERT(nvobj::string_view(it->key()).compare(std::string("ab")) ==
-		  0);
-	UT_ASSERTeq(it->value(), 1);
-
-	--it;
-	UT_ASSERT(nvobj::string_view(it->key()).compare(std::string("a")) == 0);
-	UT_ASSERTeq(it->value(), 3);
-
-	--it;
-	UT_ASSERT(nvobj::string_view(it->key()).compare(std::string("")) == 0);
-	UT_ASSERTeq(it->value(), 0);
-
-	it = r->radix_int->erase(it);
-	UT_ASSERT(nvobj::string_view(it->key()).compare(std::string("a")) == 0);
-	UT_ASSERTeq(it->value(), 3);
-
-	(*it).value() = 4;
-	UT_ASSERT(nvobj::string_view(it->key()).compare(std::string("a")) == 0);
-	UT_ASSERTeq(it->value(), 4);
-
-	it = r->radix_int->lower_bound("b");
-	UT_ASSERT(nvobj::string_view(it->key()).compare(std::string("b")) == 0);
-
-	it = r->radix_int->lower_bound("aa");
-	UT_ASSERT(nvobj::string_view(it->key()).compare(std::string("ab")) ==
-		  0);
-
-	auto it2 = r->radix_str->find("");
-
-	it2 = r->radix_str->lower_bound("aa");
-	auto it3 = it2;
-	it2.assign_val("xx");
-
-	UT_ASSERT(nvobj::string_view(it2->value()).compare("xx") == 0);
-	UT_ASSERT(nvobj::string_view(it3->value()).compare("xx") == 0);
-
-	auto long_string = std::string(1024, 'x');
-	/* The previous assignment should not invalidate the iterator. */
-	it2.assign_val(long_string);
-
-	UT_ASSERT(nvobj::string_view(it2->value()).compare(long_string) == 0);
-
-	UT_ASSERT(nvobj::string_view(r->radix_str->lower_bound("aa")->value())
-			  .compare(long_string) == 0);
-
-	UT_ASSERT(r->radix_str->find("") != r->radix_str->end());
-	UT_ASSERT(r->radix_str->find(" ") != r->radix_str->end());
-	UT_ASSERT(r->radix_str->find(" ") != r->radix_str->end());
-
-	/* Verify operator<< compiles. */
-	std::stringstream ss;
-	ss << *r->radix_str;
-
-	nvobj::transaction::run(pop, [&] {
-		nvobj::delete_persistent<container_string>(r->radix_str);
-		nvobj::delete_persistent<container_int>(r->radix_int);
-	});
-
-	UT_ASSERT(OID_IS_NULL(pmemobj_first(pop.handle())));
-}
-
-void
-test_ref_stability(nvobj::pool<root> &pop)
-{
-	auto r = pop.root();
-
-	nvobj::transaction::run(pop, [&] {
-		r->radix_str = nvobj::make_persistent<container_string>();
-		r->radix_int = nvobj::make_persistent<container_int>();
-	});
-
-	{
-		auto &ab_ref = *r->radix_str->emplace("ab", "ab").first;
-		auto &a_ref = *r->radix_str->emplace("a", "a").first;
-		auto &acxxxy_ref =
-			*r->radix_str->emplace("acxxxy", "acxxxy").first;
-		auto &acxxxz_ref =
-			*r->radix_str->emplace("acxxxz", "acxxxz").first;
-
-		UT_ASSERT(nvobj::string_view(ab_ref.value()).compare("ab") ==
-			  0);
-		UT_ASSERT(nvobj::string_view(a_ref.value()).compare("a") == 0);
-		UT_ASSERT(nvobj::string_view(acxxxy_ref.value())
-				  .compare("acxxxy") == 0);
-		UT_ASSERT(nvobj::string_view(acxxxz_ref.value())
-				  .compare("acxxxz") == 0);
-	}
-
-	{
-		auto &ab_ref = *r->radix_int->emplace("ab", 1U).first;
-		auto &a_ref = *r->radix_int->emplace("a", 2U).first;
-		auto &acxxxy_ref = *r->radix_int->emplace("acxxxy", 3U).first;
-		auto &acxxxz_ref = *r->radix_int->emplace("acxxxz", 4U).first;
-
-		UT_ASSERT(ab_ref.value() == 1U);
-		UT_ASSERT(a_ref.value() == 2U);
-		UT_ASSERT(acxxxy_ref.value() == 3U);
-		UT_ASSERT(acxxxz_ref.value() == 4U);
-	}
-
-	nvobj::transaction::run(pop, [&] {
-		nvobj::delete_persistent<container_string>(r->radix_str);
-		nvobj::delete_persistent<container_int>(r->radix_int);
-	});
-}
-
-/* Tests some corner cases (not covered by libcxx find/bound tests). */
-void
-test_find(nvobj::pool<root> &pop)
-{
-	auto r = pop.root();
-
-	{
-		nvobj::transaction::run(pop, [&] {
-			r->radix_str =
-				nvobj::make_persistent<container_string>();
-		});
-
-		UT_ASSERT(r->radix_str->lower_bound("") == r->radix_str->end());
-		UT_ASSERT(r->radix_str->upper_bound("") == r->radix_str->end());
-
-		r->radix_str->emplace("ab", "");
-		r->radix_str->emplace("a", "");
-		r->radix_str->emplace("acxxxy", "");
-		r->radix_str->emplace("acxxxz", "");
-
-		UT_ASSERT(r->radix_str->lower_bound("acxxxyy") ==
-			  r->radix_str->find("acxxxz"));
-		UT_ASSERT(r->radix_str->upper_bound("acxxxyy") ==
-			  r->radix_str->find("acxxxz"));
-
-		/* Assert no such elements. */
-		UT_ASSERT(r->radix_str->find("acxxx") == r->radix_str->end());
-		UT_ASSERT(r->radix_str->find("ac") == r->radix_str->end());
-		UT_ASSERT(r->radix_str->find("acyyy") == r->radix_str->end());
-		UT_ASSERT(r->radix_str->find("ay") == r->radix_str->end());
-		UT_ASSERT(r->radix_str->lower_bound("acxxxzz") ==
-			  r->radix_str->end());
-		UT_ASSERT(r->radix_str->upper_bound("acxxxzz") ==
-			  r->radix_str->end());
-
-		/* Find will descend to the acxxxy and fail only after comparing
-		 * keys.
-		 */
-		UT_ASSERT(r->radix_str->find("acyyyy") == r->radix_str->end());
-		UT_ASSERT(r->radix_str->find("acyyyz") == r->radix_str->end());
-
-		/* Test *_bound when leaf nodes' keys differ from the
-		 * searched-for key at compressed bytes. */
-		UT_ASSERT(r->radix_str->lower_bound("acaaay") ==
-			  r->radix_str->find("acxxxy"));
-		UT_ASSERT(r->radix_str->upper_bound("acaaay") ==
-			  r->radix_str->find("acxxxy"));
-		UT_ASSERT(r->radix_str->lower_bound("acyyyy") ==
-			  r->radix_str->end());
-		UT_ASSERT(r->radix_str->upper_bound("acyyyy") ==
-			  r->radix_str->end());
-		UT_ASSERT(r->radix_str->lower_bound("acyyy") ==
-			  r->radix_str->end());
-		UT_ASSERT(r->radix_str->upper_bound("acyyy") ==
-			  r->radix_str->end());
-
-		/* Look for key which shares some common part with leafs but
-		 * differs on compressed bytes. */
-		UT_ASSERT(r->radix_str->lower_bound("acx") ==
-			  r->radix_str->find("acxxxy"));
-		UT_ASSERT(r->radix_str->upper_bound("acx") ==
-			  r->radix_str->find("acxxxy"));
-		UT_ASSERT(r->radix_str->lower_bound("acxxx") ==
-			  r->radix_str->find("acxxxy"));
-		UT_ASSERT(r->radix_str->upper_bound("acxxx") ==
-			  r->radix_str->find("acxxxy"));
-		UT_ASSERT(r->radix_str->lower_bound("acxxa") ==
-			  r->radix_str->find("acxxxy"));
-		UT_ASSERT(r->radix_str->upper_bound("acxxa") ==
-			  r->radix_str->find("acxxxy"));
-		UT_ASSERT(r->radix_str->lower_bound("acaaa") ==
-			  r->radix_str->find("acxxxy"));
-		UT_ASSERT(r->radix_str->upper_bound("acaaa") ==
-			  r->radix_str->find("acxxxy"));
-
-		r->radix_str->emplace("ad", "");
-
-		UT_ASSERT(r->radix_str->lower_bound("acyyyy") ==
-			  r->radix_str->find("ad"));
-		UT_ASSERT(r->radix_str->upper_bound("acyyyy") ==
-			  r->radix_str->find("ad"));
-		UT_ASSERT(r->radix_str->lower_bound("acyyy") ==
-			  r->radix_str->find("ad"));
-		UT_ASSERT(r->radix_str->upper_bound("acyyy") ==
-			  r->radix_str->find("ad"));
-		UT_ASSERT(r->radix_str->lower_bound("acxxxzz") ==
-			  r->radix_str->find("ad"));
-		UT_ASSERT(r->radix_str->upper_bound("acxxxzz") ==
-			  r->radix_str->find("ad"));
-
-		nvobj::transaction::run(pop, [&] {
-			nvobj::delete_persistent<container_string>(
-				r->radix_str);
-		});
-
-		UT_ASSERT(OID_IS_NULL(pmemobj_first(pop.handle())));
-	}
-
-	{
-		nvobj::transaction::run(pop, [&] {
-			r->radix_str =
-				nvobj::make_persistent<container_string>();
-		});
-
-		r->radix_str->emplace("a", "");
-		r->radix_str->emplace("bccc", "");
-		r->radix_str->emplace("bccca", "");
-		r->radix_str->emplace("bcccbccc", "");
-
-		UT_ASSERT(r->radix_str->lower_bound("baaaba") ==
-			  r->radix_str->find("bccc"));
-		UT_ASSERT(r->radix_str->upper_bound("baaaba") ==
-			  r->radix_str->find("bccc"));
-		UT_ASSERT(r->radix_str->lower_bound("ba") ==
-			  r->radix_str->find("bccc"));
-		UT_ASSERT(r->radix_str->upper_bound("ba") ==
-			  r->radix_str->find("bccc"));
-		UT_ASSERT(r->radix_str->lower_bound("b") ==
-			  r->radix_str->find("bccc"));
-		UT_ASSERT(r->radix_str->upper_bound("b") ==
-			  r->radix_str->find("bccc"));
-		UT_ASSERT(r->radix_str->lower_bound("bddd") ==
-			  r->radix_str->end());
-		UT_ASSERT(r->radix_str->upper_bound("bddd") ==
-			  r->radix_str->end());
-
-		nvobj::transaction::run(pop, [&] {
-			nvobj::delete_persistent<container_string>(
-				r->radix_str);
-		});
-
-		UT_ASSERT(OID_IS_NULL(pmemobj_first(pop.handle())));
-	}
-}
 
 const auto compressed_path_len = 4;
 const auto num_children = 3;
@@ -326,6 +38,41 @@ generate_compressed_tree(nvobj::persistent_ptr<container_string> ptr,
 	}
 }
 
+static void
+verify_bounds(nvobj::persistent_ptr<container_string> ptr,
+	      const std::vector<std::string> &keys)
+{
+	for (size_t i = 0; i < keys.size() - 1; i++) {
+		/* generate key k for which k < keys[i] && k >= keys[i - 1] */
+		auto k = keys[i];
+		k[k.size() - 1]--;
+
+		if (i > 0)
+			UT_ASSERT(k > keys[i - 1]);
+
+		auto it = ptr->upper_bound(k);
+		UT_ASSERT(it->key() == keys[i]);
+
+		it = ptr->lower_bound(k);
+		UT_ASSERT(it->key() == keys[i]);
+	}
+}
+
+static void
+verify_bounds_key(nvobj::persistent_ptr<container_string> ptr,
+		  const std::vector<std::string> &keys, const std::string &key)
+{
+	auto expected = std::lower_bound(keys.begin(), keys.end(), key);
+	auto actual = ptr->lower_bound(key);
+	UT_ASSERT((expected == keys.end() && actual == ptr->end()) ||
+		  actual->key() == *expected);
+
+	expected = std::upper_bound(keys.begin(), keys.end(), key);
+	actual = ptr->upper_bound(key);
+	UT_ASSERT((expected == keys.end() && actual == ptr->end()) ||
+		  actual->key() == *expected);
+}
+
 void
 test_compression(nvobj::pool<root> &pop)
 {
@@ -347,6 +94,8 @@ test_compression(nvobj::pool<root> &pop)
 	std::sort(test_keys.begin(), test_keys.end());
 	UT_ASSERT(test_keys == keys);
 
+	verify_bounds(r->radix_str, keys);
+
 	for (size_t i = 1; i < keys.size() - 1; i++) {
 		/* Key consists of segments like this:
 		 * N-path-M-path ... where N, M is child number.
@@ -359,76 +108,29 @@ test_compression(nvobj::pool<root> &pop)
 
 		/* flip some bit at the end (part of a compression) */
 		k[idx] = 0;
+		verify_bounds_key(r->radix_str, keys, k);
 		auto lb = r->radix_str->lower_bound(k);
 		auto rb = r->radix_str->upper_bound(k);
 		UT_ASSERT(lb == rb);
 		UT_ASSERT(r->radix_str->find(keys[i]) == lb);
+
+		k[idx] = std::numeric_limits<char>::max();
+		verify_bounds_key(r->radix_str, keys, k);
+
+		k = keys[i];
+		k[1] = 0;
+		verify_bounds_key(r->radix_str, keys, k);
+
+		k = keys[i];
+		k[1] = std::numeric_limits<char>::max();
+		verify_bounds_key(r->radix_str, keys, k);
+
+		k = keys[i] + "postfix";
+		verify_bounds_key(r->radix_str, keys, k);
+
+		k = keys[i].substr(0, k.size() - compressed_path_len - 1);
+		verify_bounds_key(r->radix_str, keys, k);
 	}
-
-	nvobj::transaction::run(pop, [&] {
-		nvobj::delete_persistent<container_string>(r->radix_str);
-	});
-
-	UT_ASSERT(OID_IS_NULL(pmemobj_first(pop.handle())));
-}
-
-/* Tests some corner cases (not covered by libcxx erase tests). */
-void
-test_erase(nvobj::pool<root> &pop)
-{
-	auto r = pop.root();
-
-	nvobj::transaction::run(pop, [&] {
-		r->radix_str = nvobj::make_persistent<container_string>();
-	});
-
-	std::unordered_set<std::string> set = {
-		"ab", "a", "acxxxa", "acxxxb", "acxxxx", "acxxxy", "acxxxz"};
-
-	/* Used for testing iterator stability. */
-	std::unordered_map<std::string, typename container_string::iterator>
-		its;
-
-	for (auto &s : set) {
-		auto ret = r->radix_str->emplace(s, s);
-		UT_ASSERT(ret.second);
-		its.emplace(s, ret.first);
-	}
-
-	auto erase = [&](std::string key) {
-		UT_ASSERT(r->radix_str->erase(key) == 1);
-		set.erase(key);
-
-		for (auto &s : set) {
-			auto it = r->radix_str->find(s);
-			UT_ASSERT(it != r->radix_str->end());
-			UT_ASSERT(nvobj::string_view(it->value()).compare(s) ==
-				  0);
-
-			auto &m_it = its.find(s)->second;
-			UT_ASSERT(nvobj::string_view(m_it->key()).compare(s) ==
-				  0);
-			UT_ASSERT(
-				nvobj::string_view(m_it->value()).compare(s) ==
-				0);
-		}
-	};
-
-	/* Force internal node removal. */
-	erase("acxxxy");
-	erase("acxxxz");
-	erase("acxxxa");
-
-	/* At this point "acxxx" parent should have 2 children - "acxxx" and
-	 * "acxxxb". It should NOT be removed. */
-
-	erase("acxxxb");
-
-	erase("acxxxx");
-	erase("ab");
-	erase("a");
-
-	UT_ASSERT(r->radix_str->size() == 0);
 
 	nvobj::transaction::run(pop, [&] {
 		nvobj::delete_persistent<container_string>(r->radix_str);
@@ -553,10 +255,10 @@ test_pre_post_fixes(nvobj::pool<root> &pop)
 		if (i % 2 == 0)
 			elements.push_back(
 				elements.back() +
-				std::string(1, char(generator() % 128)));
+				std::string(1, char(generator() % 127 + 1)));
 		else {
 			auto str = elements.back();
-			str.back() |= (-(char(generator() % 128)));
+			str.back() |= (-(char(generator() % 127 + 1)));
 			elements.push_back(str);
 		}
 	}
@@ -582,6 +284,8 @@ test_pre_post_fixes(nvobj::pool<root> &pop)
 			its.emplace(elements[i - 1], ret.first);
 	}
 
+	verify_bounds(r->radix_str, s_elements);
+
 	UT_ASSERTeq(r->radix_str->size(), num_elements);
 	unsigned i = 0;
 	for (auto it = r->radix_str->begin(); it != r->radix_str->end();
@@ -597,34 +301,6 @@ test_pre_post_fixes(nvobj::pool<root> &pop)
 		UT_ASSERT(nvobj::string_view(it->value())
 				  .compare(s_elements[i - 1]) == 0);
 	}
-
-	nvobj::transaction::run(pop, [&] {
-		nvobj::delete_persistent<container_string>(r->radix_str);
-	});
-
-	UT_ASSERT(OID_IS_NULL(pmemobj_first(pop.handle())));
-}
-
-void
-test_assign_inline_string(nvobj::pool<root> &pop)
-{
-	const std::string test_value = "value";
-
-	auto r = pop.root();
-
-	nvobj::transaction::run(pop, [&] {
-		r->radix_str = nvobj::make_persistent<container_string>();
-		r->radix_str->try_emplace("key", test_value);
-	});
-
-	std::string new_value = "";
-	for (int i = 0; i < 1000; i++) {
-		new_value += "x";
-		r->radix_str->find("key").assign_val(new_value);
-	}
-
-	UT_ASSERT(nvobj::string_view(r->radix_str->find("key")->value())
-			  .compare(new_value) == 0);
 
 	nvobj::transaction::run(pop, [&] {
 		nvobj::delete_persistent<container_string>(r->radix_str);
@@ -656,13 +332,8 @@ test(int argc, char *argv[])
 	std::cout << "rand seed: " << seed << std::endl;
 	generator = std::mt19937_64(seed);
 
-	test_ref_stability(pop);
-	test_iterators(pop);
-	test_find(pop);
-	test_erase(pop);
 	test_binary_keys(pop);
 	test_pre_post_fixes(pop);
-	test_assign_inline_string(pop);
 	test_compression(pop);
 
 	pop.close();
