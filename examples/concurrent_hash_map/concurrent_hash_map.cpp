a8bd5498ec078d8eb62bb8c3f4785ebc8c7fad78
Igor Chorążewicz <igor.chorazewicz@intel.com>
2020-11-06 12:17:39 +0100

    XXX

--- examples/concurrent_hash_map/concurrent_hash_map.cpp
+++ examples/concurrent_hash_map/concurrent_hash_map.cpp
@@ -1,10 +1,8 @@
 // SPDX-License-Identifier: BSD-3-Clause
 /* Copyright 2019-2020, Intel Corporation */
-
 /*
  * concurrent_hash_map.cpp -- C++ documentation snippets.
  */
-
 //! [concurrent_hash_map_ex]
 #include <iostream>
 #include <libpmemobj++/container/concurrent_hash_map.hpp>
@@ -12,22 +10,78 @@
 #include <libpmemobj++/persistent_ptr.hpp>
 #include <libpmemobj++/pool.hpp>
 #include <vector>
-
+#include <chrono>
+#include <mutex>
+#include <condition_variable>
+//dennis, allocate the key/value from the memkind
+//#include "pmem_allocator.h"
+//#include <scoped_allocator>
+//#include <string>
+//#include <tbb/concurrent_hash_map.h>
 using namespace pmem::obj;
-
 /* In this example we will be using concurrent_hash_map with p<int> type for
  * both keys and values */
 using hashmap_type = concurrent_hash_map<p<int>, p<int>>;
-
-const int THREADS_NUM = 30;
-const bool remove_hashmap = false;
-
+const int THREADS_NUM = 10;
+// !!!!!!!!!!!!!!!
+bool is_write=true; 
 /* This is basic example and we only need to use concurrent_hash_map. Hence we
  * will correlate memory pool root object with single instance of persistent
  * pointer to hasmap_type */
 struct root {
 	persistent_ptr<hashmap_type> pptr;
 };
+
+std::vector<std::chrono::steady_clock::time_point> times_start;
+std::vector<std::chrono::steady_clock::time_point> times_end;
+
+template <typename Function>
+void
+parallel_exec(size_t concurrency, Function f)
+{
+	std::vector<std::thread> threads;
+	threads.reserve(concurrency);
+
+	for (size_t i = 0; i < concurrency; ++i) {
+		threads.emplace_back(f, i);
+	}
+
+	for (auto &t : threads) {
+		t.join();
+	}
+}
+
+/*
+ * This function executes 'concurrency' threads and provides
+ * 'syncthreads' method (synchronization barrier) for f()
+ */
+template <typename Function>
+void
+parallel_xexec(size_t concurrency, Function f)
+{
+	std::condition_variable cv;
+	std::mutex m;
+	std::unique_ptr<size_t> counter =
+		std::unique_ptr<size_t>(new size_t(0));
+
+	auto syncthreads = [&] {
+		std::unique_lock<std::mutex> lock(m);
+		(*counter)++;
+		if (*counter < concurrency)
+			cv.wait(lock, [&] { return *counter >= concurrency; });
+		else
+			/*
+			 * notify_call could be called outside of a lock
+			 * (it would perform better) but drd complains
+			 * in that case
+			 */
+			cv.notify_all();
+	};
+
+	parallel_exec(concurrency, [&](size_t tid) { f(tid, syncthreads); });
+}
+
+
 
 /* Before running this example, run:
  * pmempool create obj --layout="concurrent_hash_map" --size 1G path_to_a_pool
@@ -36,156 +90,98 @@ int
 main(int argc, char *argv[])
 {
 	pool<root> pop;
-	try {
-		if (argc != 2)
-			std::cerr << "usage: " << argv[0] << " file-name"
-				  << std::endl;
-
-		auto path = argv[1];
-
 		try {
-			pop = pool<root>::open(path, "concurrent_hash_map");
+			pop = pool<root>::open("/dev/shm/c1", "concurrent_hash_map");
 		} catch (pmem::pool_error &e) {
 			std::cerr << e.what() << std::endl;
 			return -1;
 		}
-
 		auto &r = pop.root()->pptr;
-
 		if (r == nullptr) {
-			/* Logic when file was first opened. First, we have to
-			 * allocate object of hashmap_type and attach it to the
-			 * root object. */
+			printf("the root is not allocated..\n");
 			pmem::obj::transaction::run(pop, [&] {
 				r = make_persistent<hashmap_type>();
 			});
-
 			r->runtime_initialize();
 		} else {
 			/* Logic when hash_map already exists. After opening of
 			 * the pool we have to call runtime_initialize()
 			 * function in order to recalculate mask and check for
 			 * consistentcy. */
-
+			printf("the root is allocated\n");
 			r->runtime_initialize();
-
-			/* defragment the whole pool at the beginning */
-			try {
-				r->defragment();
-			} catch (const pmem::defrag_error &e) {
-				std::cerr << "Defragmentation exception: "
-					  << e.what() << std::endl;
-				throw;
-			}
 		}
-
 		auto &map = *r;
-		std::cout << map.size() << std::endl;
-
 		std::vector<std::thread> threads;
 		threads.reserve(static_cast<size_t>(THREADS_NUM));
-
-		/* Insert THREADS_NUM / 3 key-value pairs to the hashmap. This
+		long WRITE_SIZE  = 1e6;
+		long READ_SIZE = WRITE_SIZE;
+		clock_t start, end;
+		int k=map.size();
+		//dennis check the keys restores or not??
+		//printf("the items account=%d\n", std::distance(map.begin(), map.end()));
+		if(k!=0) is_write=false;
+		else is_write=true;		
+		// write 
+		/* Insert WRITE_SIZE key-value pairs to the hashmap. This
 		 * operation is thread-safe. */
-		for (int i = 0; i < THREADS_NUM / 3; ++i) {
-			threads.emplace_back([&]() {
-				for (int i = 0; i < 10 * THREADS_NUM; ++i) {
-					map.insert(
-						hashmap_type::value_type(i, i));
-				}
-			});
-		}
-
-		/* Erase THREADS_NUM /3 key-value pairs from the hashmap. This
-		 * operation is thread-safe. */
-		for (int i = 0; i < THREADS_NUM / 3; ++i) {
-			threads.emplace_back([&]() {
-				for (int i = 0; i < 10 * THREADS_NUM; ++i) {
-					map.erase(i);
-				}
-			});
-		}
-
-		/* Check if given key is in the hashmap. For the time of an
-		 * accessor life, the read-write lock is taken on the item. */
-		for (int i = 0; i < THREADS_NUM / 3; ++i) {
-			threads.emplace_back([&]() {
-				for (int i = 0; i < 10 * THREADS_NUM; ++i) {
-					hashmap_type::accessor acc;
-					bool res = map.find(acc, i);
-
-					if (res) {
-						assert(acc->first == i);
-						assert(acc->second >= i);
-						acc->second.get_rw() += 1;
-						pop.persist(acc->second);
+		if (is_write)
+		{
+			start = clock();
+			for (int thr = 0; thr < THREADS_NUM; thr++) {	
+				int t = thr;
+				threads.emplace_back([&, t]() {
+					for (int i = t*WRITE_SIZE/THREADS_NUM; i < (t+1)*WRITE_SIZE/THREADS_NUM; i++) {
+						map.insert(
+							hashmap_type::value_type(i, i));
 					}
-				}
-			});
-		}
-
-		for (auto &t : threads) {
-			t.join();
-		}
-		try {
-			/* defragment the whole pool at the end */
-			map.defragment();
-		} catch (const pmem::defrag_error &e) {
-			std::cerr << "Defragmentation exception: " << e.what()
-				  << std::endl;
-			throw;
-		} catch (const pmem::lock_error &e) {
-			std::cerr << "Defragmentation exception: " << e.what()
-				  << std::endl;
-			throw;
-		} catch (const std::range_error &e) {
-			std::cerr << "Defragmentation exception: " << e.what()
-				  << std::endl;
-			throw;
-		} catch (const std::runtime_error &e) {
-			std::cerr << "Defragmentation exception: " << e.what()
-				  << std::endl;
-			throw;
-		}
-		/* If hash map is to be removed, free_data() method should be
-		 * called first. Otherwise, if deallocating internal hash map
-		 * metadata in a destructor fail program might terminate. */
-		if (remove_hashmap) {
-			/* Erase remaining itemes in map. This function is not
-			 * thread-safe, hence the function is being called only
-			 * after thread execution has completed. */
-			try {
-				map.clear();
-			} catch (const pmem::transaction_out_of_memory &e) {
-				std::cerr << "Clear exception: " << e.what()
-					  << std::endl;
-				throw;
-			} catch (const pmem::transaction_free_error &e) {
-				std::cerr << "Clear exception: " << e.what()
-					  << std::endl;
-				throw;
+				});
 			}
-
-			map.free_data();
-
-			/* map.clear() // WRONG
-			 * After free_data() concurrent hash map cannot be used
-			 * anymore! */
-
-			transaction::run(pop, [&] {
-				delete_persistent<hashmap_type>(r);
-			});
+			for (auto &t : threads) {
+				t.join();
+			}
+			end = clock();
+			printf("write avg time = %lf us \n", 
+					double(end - start) * 1e6 / CLOCKS_PER_SEC / WRITE_SIZE);  // 1M us = 1s
+			threads.clear();
 		}
+		std::cout << "items count " <<  map.size() << std::endl;
+
+		times_start.resize(THREADS_NUM);
+		times_end.resize(THREADS_NUM);
+
+		parallel_xexec(THREADS_NUM, [&](size_t thread_id, std::function<void(void)> syncthreads){
+			syncthreads();
+			times_start[thread_id] = std::chrono::steady_clock::now();
+			for (size_t i = thread_id*READ_SIZE/THREADS_NUM; i < (thread_id+1)*READ_SIZE/THREADS_NUM; i++) {
+				hashmap_type::accessor acc;
+				bool res = map.find(acc, i);
+				if (res) {
+				}
+			}
+			times_end[thread_id] = std::chrono::steady_clock::now();
+			syncthreads();
+		});
+
+		auto min = std::min_element(times_start.begin(), times_start.end());
+		auto max = std::max_element(times_end.begin(), times_end.end());
+		
+		std::cout << "duration " << std::chrono::duration_cast<std::chrono::milliseconds>(
+			*max - *min).count() << std::endl;
+
+		size_t sum = 0;
+
+		for (int i = 0; i < THREADS_NUM; i++) {
+			auto secs = std::chrono::duration_cast<std::chrono::microseconds>(
+			times_end[i] - times_start[i]).count();
+
+			sum += secs;
+		}
+
+		std::cout << "sum " << sum << std::endl;
+
 		pop.close();
-	} catch (std::exception &e) {
-		std::cerr << "Exception occured: " << e.what() << std::endl;
-		try {
-			pop.close();
-		} catch (const std::logic_error &e) {
-			std::cerr << "Exception: " << e.what() << std::endl;
-		}
-		return -1;
-	}
+
 	return 0;
 }
 //! [concurrent_hash_map_ex]
